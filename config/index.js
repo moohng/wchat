4ad3e426eb4a550d8b7650aa5063381139de8648
mohong <moohng@126.com>
2019-08-08 16:26:21 +0800

    travis环境

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: process.env.NODE_CI === 'travis' ? '/wchat/' : '/',
 
     /**
      * Source Maps
