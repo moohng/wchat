1ee26a29f12729f063916f550b8d97227f39a210
mohong <moohng@126.com>
2019-08-14 16:44:35 +0800

    update travis

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/wchat/' : '/',
   configureWebpack: config => {
     require('@vux/loader').merge(config, {
       plugins: ['vux-ui']
